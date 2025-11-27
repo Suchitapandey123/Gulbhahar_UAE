1903ff65448520997df89cd04f0faed6c2030be7
Akash Pandey <akkushakku5143@gmail.com>
2025-11-27 12:53:11 +0530

    fixed update Profile

--- src/utils/envHere.js
+++ src/utils/envHere.js
@@ -1,6 +1,6 @@
-// export const API_BASE_URL = 'https://api.gulbhahar.com';
+export const API_BASE_URL = 'https://api.gulbhahar.com';
 // export const API_BASE_URL = "http://localhost:8080"
-export const API_BASE_URL = "http://194.238.23.44:9080"
+// export const API_BASE_URL = "http://194.238.23.44:9080"
 // TEST_GULBHAHAR_BACKEND=194.238.23.44:9080
 
 export default {
